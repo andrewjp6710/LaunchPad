602c1f9f7dc512e8baae3ada241eca6b4be79ce1
Jamie Domingo <jamiedomingo@JCD-Macbook.local>
2021-07-10 13:21:45 +1000

    adding react router to dependencies

--- frontend/launchpad-app/src/App.js
+++ frontend/launchpad-app/src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 
+import Button from '@material-ui/core/Button';
+
 function App() {
-  const date = Date.now()
+  /* const date = Date.now()
   const bool = true;
   const [loggedIn, setLoggedIn] = useState(false);
   
@@ -14,12 +16,14 @@ function App() {
     // set state to fetched 
   }
 
-
+ */
 
   return (
     <div className="App">
       <p>Hello, world</p>
-
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
     </div>
   );
 }
