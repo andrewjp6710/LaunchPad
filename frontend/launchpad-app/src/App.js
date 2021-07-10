8288143f0f7a5686cc75a37302033b9a094f7df4
Jamie Domingo <jamiedomingo@JCD-Macbook.local>
2021-07-10 13:43:56 +1000

    Added router switch

--- frontend/launchpad-app/src/App.js
+++ frontend/launchpad-app/src/App.js
@@ -1,30 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
-
-import Button from '@material-ui/core/Button';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Homepage } from './pages/HomePage';
 
 function App() {
-  /* const date = Date.now()
-  const bool = true;
-  const [loggedIn, setLoggedIn] = useState(false);
-  
-  
-
-  function checkLoggedIn() {
-    // fetch backend endpoint
-    // set state to fetched 
-  }
-
- */
 
   return (
-    <div className="App">
-      <p>Hello, world</p>
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        
+        <Switch>
+          {/* <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route> */}
+          <Route path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
