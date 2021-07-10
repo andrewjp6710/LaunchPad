9cae3af449c1094058677e6ba5b4f2edc70d4379
Eugenie Hsieh <eugenieh@192-168-1-111.tpgi.com.au>
2021-07-10 14:06:18 +1000

    add Header Footer

--- frontend/launchpad-app/src/App.js
+++ frontend/launchpad-app/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
 
 function App() {
+  /*
   const date = Date.now()
   const bool = true;
   const [loggedIn, setLoggedIn] = useState(false);
@@ -13,13 +15,45 @@ function App() {
     // fetch backend endpoint
     // set state to fetched 
   }
+  */
 
 
 
   return (
     <div className="App">
-      <p>Hello, world</p>
+      <main className="background">
+        <header>
+          <p className="header-logo">
+            launchpad.
+          </p>
 
+          <p className="header-signin">
+            <Button
+                style={{
+                    fontWeight: "bold",
+                    fontSize: "14px",
+                    color: "#E4816B",
+                    borderColor: "#E4816B",
+                    borderWidth: 2
+                }}
+                variant="outlined">
+                Sign In
+            </Button> 
+          </p>
+        </header>
+
+      </main>
+
+
+      <footer className="footer-background">
+        <p className="footer-details">
+          Accenture Team 1
+        </p>
+      </footer>
+        
+       
+
+    
     </div>
   );
 }
